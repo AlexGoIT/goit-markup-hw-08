ab92fd3cdcf4a31484012485bd89ab6a1d32e593
AlexGoIT <alexandrpuryaev@gmail.com>
2023-04-21 14:55:17 +0300

    Correct filter

--- js/filter.js
+++ js/filter.js
@@ -2,12 +2,12 @@ const filter = document.querySelector('.filter-items');
 const portfolioCards = document.querySelectorAll('.portfolio-card-item');
 
 filter.addEventListener('click', e => {
-  const filterValue = e.target.dataset.filter;
+  const value = e.target.textContent.toLowerCase();
 
   portfolioCards.forEach(item => {
     const descript = item.querySelector('.card-descript');
 
-    if (descript.textContent.toLowerCase().includes(filterValue) || filterValue === 'all') {
+    if (descript.textContent.toLowerCase().includes(value) || value === 'all') {
       item.classList.remove('hide');
     } else {
       item.classList.add('hide');
